18c8559bb88a9e221b984403bc83e7bd9552c360
lvzhenpeng <lvzhenpeng@pansoft.com>
2023-04-18 18:20:38 +0800

    去除无用引用

--- src/main/java/com/pansoft/config/HazelcastConfiguration.java
+++ src/main/java/com/pansoft/config/HazelcastConfiguration.java
@@ -23,47 +23,6 @@ import java.util.*;
 @Configuration
 public class HazelcastConfiguration {
 
-   /* @Bean
-    public Config hazelCastConfig() {
-        List<String> memberList = new ArrayList<>();
-        memberList.add("172.17.60.22");
-        Config config = new Config();
-        NetworkConfig networkConfig = new NetworkConfig();
-        networkConfig.setInterfaces(new InterfacesConfig().setInterfaces(memberList));
-        networkConfig.setPort(20400);
-        networkConfig.setPortAutoIncrement(true);
-        networkConfig.setPortCount(100);
-        networkConfig.setOutboundPorts(Collections.singletonList(0));
-        networkConfig.setJoin(new JoinConfig()
-                .setMulticastConfig(new MulticastConfig().setEnabled(false))
-                .setTcpIpConfig(new TcpIpConfig().setEnabled(true).setMembers(memberList).setRequiredMember(null).setConnectionTimeoutSeconds(5)));
-        Map<String, ExecutorConfig> map = new HashMap<>();
-        map.put("default", new ExecutorConfig().setPoolSize(16).setQueueCapacity(0));
-        List<EntryListenerConfig> entryListenerConfigList = new ArrayList<>();
-        EntryListenerConfig entryListenerConfig = new EntryListenerConfig();
-        entryListenerConfig.setClassName("com.palm.vert.cluster.listener.ClusterCacheListener");
-        entryListenerConfigList.add(entryListenerConfig);
-        config.setInstanceName("hazelcast-instance")
-                .addMapConfig(new MapConfig().setName("configuration").setEvictionConfig(new EvictionConfig().setSize(200).setMaxSizePolicy(MaxSizePolicy.FREE_HEAP_SIZE)
-                                .setEvictionPolicy(EvictionPolicy.LFU))
-                        .setTimeToLiveSeconds(-1).setEntryListenerConfigs(entryListenerConfigList))
-                .setManagementCenterConfig(new ManagementCenterConfig().setConsoleEnabled(false))
-                .setNetworkConfig(networkConfig)
-                .setPartitionGroupConfig(new PartitionGroupConfig().setEnabled(false))
-                .setExecutorConfigs(map);
-        return config;
-    }*/
-
-/*    @Bean
-    public Config hazelcastConfig() {
-        Config config = new Config();
-        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
-        config.getNetworkConfig().getJoin().getEurekaConfig()
-                .setEnabled(true)
-                .setProperty("self-registration", "true")
-                .setProperty("namespace", "hazelcast");
-        return config;
-    }*/
 
     @Bean
     public HazelcastInstance hazelcastConfig() {
